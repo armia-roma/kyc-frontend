48c420b7b453ee2b11cd96506041db2f7070b440
armia-roma <armia_bos@gmail.com>
2025-01-22 17:57:53 +0400

    add vue route , vuetify

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-app>
+    <v-layout>
+      <v-main>
+        <RouterView />
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
+
+<style scoped></style>
