0d92bbfb8dde8dfd499c095b5979194d357f1865
armia-roma <armia_bos@gmail.com>
2025-01-23 00:24:19 +0400

    add loading to  login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,11 +2,18 @@
 import { ref } from 'vue'
 const email = ref('')
 const password = ref('')
-
+const loading = ref(false)
 import { login } from './../../service/authService'
 
-function handleSubmit() {
-  login({ email: email.value, password: password.value }).then()
+async function handleSubmit() {
+  loading.value = true
+  try {
+    await login({ email: email.value, password: password.value })
+  } catch (err) {
+    loading.value = false
+  } finally {
+    loading.value = false
+  }
 }
 const nameRules = [
   (value: string) => {
@@ -25,37 +32,44 @@ const passwordRules = [
 const valid = false
 </script>
 <template>
-  <v-container fluid>
-    <v-row justify="center" align="center">
-      <v-col>
-        <v-form @submit.prevent="handleSubmit" v-model="valid">
-          <v-row>
-            <v-col cols="12" md="4">
-              <v-text-field
-                variant="outlined"
-                label="Email"
-                v-model="email"
-                :rules="nameRules"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" md="4">
-              <v-text-field
-                label="Password"
-                variant="outlined"
-                :rules="passwordRules"
-                v-model="password"
-                type="password"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12" md="4">
-              <v-btn block flat color="gray" variant="outlined" type="submit"> Login </v-btn>
-            </v-col>
-          </v-row>
-        </v-form>
+  <v-container class="fill-height d-flex align-center justify-center">
+    <v-row class="">
+      <v-col class="d-flex align-center">
+        <v-card class="pa-4 mx-auto" width="100%" max-width="500" title="Login" :loading="loading">
+          <v-form @submit.prevent="handleSubmit" v-model="valid">
+            <v-card>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    variant="outlined"
+                    label="Email"
+                    hide-details="auto"
+                    v-model="email"
+                    :rules="nameRules"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Password"
+                    variant="outlined"
+                    :rules="passwordRules"
+                    v-model="password"
+                    type="password"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12">
+                  <v-card-actions>
+                    <v-btn block color="primary" type="submit">Login</v-btn>
+                  </v-card-actions>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-form>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
