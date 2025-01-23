766c44c3f825c5e602a60d42727595bc31e9ea86
armia-roma <armia_bos@gmail.com>
2025-01-23 19:11:13 +0400

    add route gard to router index

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import apiClient from '../../service/api-client'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
-    { path: '/login', name: 'login', component: LoginView },
-  ],
+  routes: [{ path: '/login', name: 'login', component: LoginView }],
+})
+router.beforeEach(async (to, from) => {
+  const token = localStorage.getItem('token')
+  let isAuthenticated = false
+  let userRole = null
+
+  if (token) {
+    try {
+      const userData = await apiClient.post('/user/verify-token', { token })
+      isAuthenticated = true
+      userRole = userData.data.data.role
+    } catch (error) {
+      localStorage.removeItem('token')
+    }
+  }
+
+  if (!isAuthenticated && to.name !== 'login') {
+    return { name: 'login' }
+  }
+  if (isAuthenticated && userRole === 'user' && to.name !== 'kycCreate') {
+    return { name: 'kycCreate' }
+  }
 })
 
 export default router
