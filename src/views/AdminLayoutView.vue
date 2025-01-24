45e1f1f4e925d959a78deed72941b8928ff7afe0
armia-roma <armia_bos@gmail.com>
2025-01-24 14:25:00 +0400

    edit admin layout remove fix width issue

--- src/views/AdminLayoutView.vue
+++ src/views/AdminLayoutView.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts"></script>
 <template>
-  <v-container>
-    <v-navigation-drawer color="grey-lighten-5" class="text-grey-lighten-1" width="150" permanent>
-      <v-list-item link title="Create Kyc" to="/kyc-create"></v-list-item>
-      <v-list-item link title="List kyc" to="/kyc-list"></v-list-item>
-      <v-list-item link title="Admin" to="/report"></v-list-item>
-    </v-navigation-drawer>
-    <v-main>
-      <RouterView />
-    </v-main>
-  </v-container>
+	<v-container>
+		<v-navigation-drawer
+			color="grey-lighten-5"
+			class="text-grey-lighten-1"
+			width="150"
+			permanent
+		>
+			<v-list-item link title="Create Kyc" to="/kyc-create"></v-list-item>
+			<v-list-item link title="List kyc" to="/kyc-list"></v-list-item>
+			<v-list-item link title="Admin" to="/report"></v-list-item>
+		</v-navigation-drawer>
+		<v-main>
+			<RouterView />
+		</v-main>
+	</v-container>
 </template>
