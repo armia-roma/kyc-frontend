f2d93fa8f2152c77fb4a99f018c7fa524a960c0f
armia-roma <armia_bos@gmail.com>
2025-01-24 13:19:37 +0400

    add admin layout

---
+++ src/views/AdminLayoutView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <v-container>
+    <v-navigation-drawer color="grey-lighten-5" class="text-grey-lighten-1" width="150" permanent>
+    </v-navigation-drawer>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-container>
+</template>
