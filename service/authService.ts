df9fbd4c6ace0b0e789aeaf48826eb40271c59e1
armia-roma <armia_bos@gmail.com>
2025-01-26 00:54:07 +0400

    edit : add user role to local storage , simplify register method

--- service/authService.ts
+++ service/authService.ts
@@ -1,27 +1,17 @@
-import apiClient from './api-client'
+import apiClient from "./api-client";
 
 export const login = async (data: { email: string; password: string }) => {
-  try {
-    const response = await apiClient.post('/user/login', data)
-    const token = response.data.data.token
-    localStorage.setItem('token', token)
-
-    return response.data.data
-  } catch (error) {
-    console.error('Login failed:', error)
-    return error
-  }
-}
+	const response = await apiClient.post("/user/login", data);
+	const token = response.data.user.token;
+	localStorage.setItem("token", token);
+	localStorage.setItem("userRole", response.data.user.role);
+	return response.data;
+};
 export const register = async (data: {
-  userName: string
-  email: string
-  password: string
-  role: string
+	userName: string;
+	email: string;
+	password: string;
+	role: string;
 }) => {
-  try {
-    const response = await apiClient.post('/user/register', data)
-    return response.data.data
-  } catch (error) {
-    return error
-  }
-}
+	return apiClient.post("/user/register", data);
+};
