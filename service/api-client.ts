f46016fe0dbeb0a8a45c83893bf1f917c03ed5c2
armia-roma <armia_bos@gmail.com>
2025-01-26 00:58:50 +0400

    add  response interceptor handle Unauthorized error

--- service/api-client.ts
+++ service/api-client.ts
@@ -1,19 +1,33 @@
-import axios from 'axios'
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api/',
-})
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  },
-)
+	baseURL: "http://localhost:3000/api/",
+});
 
-export default apiClient
+apiClient.interceptors.request.use(
+	(config) => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			config.headers["Authorization"] = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+apiClient.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	(error) => {
+		if (error.response && error.response.status === 401) {
+			localStorage.removeItem("token");
+
+			window.location.href = "/login";
+		}
+
+		return Promise.reject(error);
+	}
+);
+export default apiClient;
